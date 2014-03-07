b17586135d71c18049c86f914ae6d54527e7f5f4
Alexis Algoud <alexis@atm-consulting.fr>
2014-03-07 18:56:27 +0100

    correction requete

--- script/import-from-milestone.php
+++ script/import-from-milestone.php
@@ -5,22 +5,25 @@
 	$sql = "UPDATE ".MAIN_DB_PREFIX."propaldet 
 		SET special_code=104777
 		, label=(SELECT label FROM ".MAIN_DB_PREFIX."milestone WHERE elementtype='propal' AND fk_element=".MAIN_DB_PREFIX."propaldet.rowid)
+		,qty=1
 	 WHERE special_code=1790";
 	 
-	 print $sql."<br />";
+	 print $sql.";<br />";
 	
 	$sql = "UPDATE ".MAIN_DB_PREFIX."facturedet 
 		SET special_code=104777
 		, label=(SELECT label FROM ".MAIN_DB_PREFIX."milestone WHERE elementtype='facture' AND fk_element=".MAIN_DB_PREFIX."facturedet.rowid)
+		,qty=1
 	 WHERE special_code=1790";
 	 
-	 print $sql."<br />";
+	 print $sql.";<br />";
 	
 	$sql = "UPDATE ".MAIN_DB_PREFIX."commandedet 
 		SET special_code=104777
 		, label=(SELECT label FROM ".MAIN_DB_PREFIX."milestone WHERE elementtype='commande' AND fk_element=".MAIN_DB_PREFIX."commandedet.rowid)
+		,qty=1
 	 WHERE special_code=1790";
 	 
-	 print $sql."<br />";
+	 print $sql.";<br />";
 	
 	
