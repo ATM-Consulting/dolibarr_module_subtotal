f5bfde341353125db8ab2d17daf5279bee1ead45
root <root@srv13.atm-consulting.fr>
2016-04-20 11:37:22 +0200

    FIX import for 3.9 and line ordering

---
+++ script/import-from-milestone-b3.8.php
@@ -0,0 +1,47 @@
+<?php
+
+	require('../config.php');
+
+	$sql = "UPDATE ".MAIN_DB_PREFIX."propaldet 
+		SET special_code=104777
+		, label=(SELECT label FROM ".MAIN_DB_PREFIX."milestone WHERE elementtype='propal' AND fk_element=".MAIN_DB_PREFIX."propaldet.rowid)
+		, qty=1
+	 WHERE special_code=1790";
+	 
+	 print $sql.";<br />";
+	
+	$sql = "UPDATE ".MAIN_DB_PREFIX."propaldet 
+		SET fk_parent_line=0
+	 WHERE 1";
+	 
+	 print $sql.";<br />";
+	
+	$sql = "UPDATE ".MAIN_DB_PREFIX."facturedet 
+		SET special_code=104777
+		, label=(SELECT label FROM ".MAIN_DB_PREFIX."milestone WHERE elementtype='facture' AND fk_element=".MAIN_DB_PREFIX."facturedet.rowid)
+		,qty=1
+	 WHERE special_code=1790";
+	 
+	 print $sql.";<br />";
+
+	$sql = "UPDATE ".MAIN_DB_PREFIX."facturedet 
+		SET fk_parent_line=0
+	 WHERE 1";
+	 
+	 print $sql.";<br />";
+
+	
+	$sql = "UPDATE ".MAIN_DB_PREFIX."commandedet 
+		SET special_code=104777
+		, label=(SELECT label FROM ".MAIN_DB_PREFIX."milestone WHERE elementtype='commande' AND fk_element=".MAIN_DB_PREFIX."commandedet.rowid)
+		,qty=1
+	 WHERE special_code=1790";
+	 
+	 print $sql.";<br />";
+	
+	$sql = "UPDATE ".MAIN_DB_PREFIX."commandedet 
+		SET fk_parent_line=0
+	 WHERE 1";
+	 
+	 print $sql.";<br />";
+	
--- script/import-from-milestone.php
+++ script/import-from-milestone.php
@@ -4,28 +4,26 @@
 
 	$sql = "UPDATE ".MAIN_DB_PREFIX."propaldet 
 		SET special_code=104777
-		, label=(SELECT label FROM ".MAIN_DB_PREFIX."milestone WHERE elementtype='propal' AND fk_element=".MAIN_DB_PREFIX."propaldet.rowid)
 		, qty=1
 	 WHERE special_code=1790";
 	 
 	 print $sql.";<br />";
 	
 	$sql = "UPDATE ".MAIN_DB_PREFIX."propaldet 
-		SET fk_parent_line=0
+		SET fk_parent_line=NULL
 	 WHERE 1";
 	 
 	 print $sql.";<br />";
 	
 	$sql = "UPDATE ".MAIN_DB_PREFIX."facturedet 
 		SET special_code=104777
-		, label=(SELECT label FROM ".MAIN_DB_PREFIX."milestone WHERE elementtype='facture' AND fk_element=".MAIN_DB_PREFIX."facturedet.rowid)
 		,qty=1
 	 WHERE special_code=1790";
 	 
 	 print $sql.";<br />";
 
 	$sql = "UPDATE ".MAIN_DB_PREFIX."facturedet 
-		SET fk_parent_line=0
+		SET fk_parent_line=NULL
 	 WHERE 1";
 	 
 	 print $sql.";<br />";
@@ -33,15 +31,59 @@
 	
 	$sql = "UPDATE ".MAIN_DB_PREFIX."commandedet 
 		SET special_code=104777
-		, label=(SELECT label FROM ".MAIN_DB_PREFIX."milestone WHERE elementtype='commande' AND fk_element=".MAIN_DB_PREFIX."commandedet.rowid)
 		,qty=1
 	 WHERE special_code=1790";
 	 
 	 print $sql.";<br />";
 	
 	$sql = "UPDATE ".MAIN_DB_PREFIX."commandedet 
-		SET fk_parent_line=0
+		SET fk_parent_line=NULL
 	 WHERE 1";
-	 
+
+
+	$res = $db->query("SELECT DISTINCT(fk_propal) as fk_propal FROM ".MAIN_DB_PREFIX."propaldet WHERE special_code=104777");
+	while($obj = $db->fetch_object($res)) {
+		$resLine = $db->query("SELECT rowid FROM  ".MAIN_DB_PREFIX."propaldet WHERE fk_propal=".$obj->fk_propal." ORDER BY rang ");
+		$k = 1;
+		while($objLine = $db->fetch_object($resLine)) {
+
+			$sql="UPDATE ".MAIN_DB_PREFIX."propaldet SET rang=".$k.",fk_parent_line=NULL WHERE rowid=".$objLine->rowid;
+
+			 print $sql.";<br />";
+			$k++;
+		}
+
+	}
+
+
+        $res = $db->query("SELECT DISTINCT(fk_facture) as fk_facture FROM ".MAIN_DB_PREFIX."facturedet WHERE special_code=104777");
+        while($obj = $db->fetch_object($res)) {
+                $resLine = $db->query("SELECT rowid FROM  ".MAIN_DB_PREFIX."facturedet WHERE fk_facture=".$obj->fk_propal." ORDER BY rang ");
+                $k = 1;
+                while($objLine = $db->fetch_object($resLine)) {
+
+                        $sql="UPDATE ".MAIN_DB_PREFIX."facturedet SET rang=".$k.",fk_parent_line=NULL WHERE rowid=".$objLine->rowid;
+
+                         print $sql.";<br />";
+                        $k++;
+                }
+
+        }
+
+        $res = $db->query("SELECT DISTINCT(fk_commande) as fk_commande FROM ".MAIN_DB_PREFIX."commandedet WHERE special_code=104777");
+        while($obj = $db->fetch_object($res)) {
+                $resLine = $db->query("SELECT rowid FROM  ".MAIN_DB_PREFIX."commandedet WHERE fk_commande=".$obj->fk_propal." ORDER BY rang ");
+                $k = 1;
+                while($objLine = $db->fetch_object($resLine)) {
+
+                        $sql="UPDATE ".MAIN_DB_PREFIX."commandedet SET rang=".$k.",fk_parent_line=NULL WHERE rowid=".$objLine->rowid;
+
+                         print $sql.";<br />";
+                        $k++;
+                }
+
+        }
+
+
 	 print $sql.";<br />";
 	
