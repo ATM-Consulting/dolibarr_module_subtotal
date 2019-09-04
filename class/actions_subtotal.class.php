d24bbf6f2215aa40237166762b102910cec15bc6
Florian Mortgat <florian.mortgat@atm-consulting.fr>
2019-09-04 14:41:24 +0200

    FIX affmarges: more robust / explicit detection + less confusing var names

--- class/actions_subtotal.class.php
+++ class/actions_subtotal.class.php
@@ -2063,9 +2063,11 @@ class ActionsSubtotal
 				$colspan++; // Colonne PU Devise
 			}
 			if($object->element == 'commande' && $object->statut < 3 && !empty($conf->shippableorder->enabled)) $colspan++;
-			if(!empty($conf->margin->enabled) && false !== $_SESSION['marginsdisplayed']) $colspan++;
-			if(!empty($conf->global->DISPLAY_MARGIN_RATES) && false !== $_SESSION['marginsdisplayed']) $colspan++;
-			if(!empty($conf->global->DISPLAY_MARK_RATES) && false !== $_SESSION['marginsdisplayed']) $colspan++;
+
+			$margins_hidden_by_module = empty($conf->affmarges->enabled) ? false : !($_SESSION['marginsdisplayed']);
+			if(!empty($conf->margin->enabled) && !$margins_hidden_by_module) $colspan++;
+			if(!empty($conf->global->DISPLAY_MARGIN_RATES) && !$margins_hidden_by_module) $colspan++;
+			if(!empty($conf->global->DISPLAY_MARK_RATES) && !$margins_hidden_by_module) $colspan++;
 			if($object->element == 'facture' && !empty($conf->global->INVOICE_USE_SITUATION) && $object->type == Facture::TYPE_SITUATION) $colspan++;
 			if(!empty($conf->global->PRODUCT_USE_UNITS)) $colspan++;
 
