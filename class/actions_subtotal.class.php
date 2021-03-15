d45434d11107bc365a40601e3a38e9369ab3dc34
lvessiller-opendsi <lvessiller@open-dsi.fr>
2021-03-15 14:22:38 +0100

    FIX exclude virtual products in total
    FIX exclude virtual products in total
    - no need to add virtual products amounts in total (already computed in product line amount)

--- class/actions_subtotal.class.php
+++ class/actions_subtotal.class.php
@@ -893,10 +893,12 @@ class ActionsSubtotal
                 }
                 else
                 {
-                    $total += $l->total_ht;
-                    $total_tva += $l->total_tva;
-                    $TTotal_tva[$l->tva_tx] += $l->total_tva;
-                    $total_ttc += $l->total_ttc;
+			if ($l->product_type != 9) {
+                    		$total += $l->total_ht;
+                    		$total_tva += $l->total_tva;
+                    		$TTotal_tva[$l->tva_tx] += $l->total_tva;
+                    		$total_ttc += $l->total_ttc;
+			}
                 }
             }
 		}
