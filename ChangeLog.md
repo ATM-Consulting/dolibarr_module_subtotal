4415ed089acf0f4df1f5e9218f0795dffac2de78
kevin <kevin.giuga@atm-consulting.fr>
2021-05-05 14:37:01 +0200

    V13 Compatibility - No token renewal

--- ChangeLog.md
+++ ChangeLog.md
@@ -6,5 +6,6 @@
 
 ## Version 3.2
 
+- FIX : Add no token renewal  *05/05/2021* - 3.2.3
 - FIX : compliance of config.php with dolistore rules *29/04/2021* - 3.2.2
 - FIX : Text or title line break PDF *15/04/2021* - 3.2.1
--- script/interface.php
+++ script/interface.php
@@ -1,6 +1,8 @@
 <?php
+	if (! defined('NOTOKENRENEWAL')) define('NOTOKENRENEWAL','1'); // Disables token renewal
+
 	require '../config.php';
-	
+
 	dol_include_once('/subtotal/lib/subtotal.lib.php');
 	dol_include_once('/subtotal/class/subtotal.class.php');
 	dol_include_once('/comm/propal/class/propal.class.php');
@@ -9,19 +11,19 @@
 	dol_include_once('/fourn/class/fournisseur.commande.class.php');
 	dol_include_once('/supplier_proposal/class/supplier_proposal.class.php');
 	dol_include_once('/fourn/class/fournisseur.facture.class.php');
-	
+
 	$get=GETPOST('get');
 	$set=GETPOST('set');
-	
+
 	switch ($get) {
 		default:
 			break;
 	}
-	
+
 	switch ($set) {
 		case 'updateLineNC': // Gestion du Compris/Non Compris via les titres et/ou lignes
 			echo json_encode( _updateLineNC(GETPOST('element'), GETPOST('elementid'), GETPOST('lineid'), GETPOST('subtotal_nc')) );
-			
+
 			break;
 		default:
 			break;
