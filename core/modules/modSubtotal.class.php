b88292bcb5cbe6a1190fb192059e0e0dd156c1dd
Adrien Raze <adrien.raze@atm-consulting.fr>
2023-06-29 15:29:51 +0200

    Num version

--- core/modules/modSubtotal.class.php
+++ core/modules/modSubtotal.class.php
@@ -67,7 +67,7 @@ class modSubtotal extends DolibarrModules
         // Possible values for version are: 'development', 'experimental' or version
 
 
-        $this->version = '3.20.0';
+        $this->version = '3.21.0';
 
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
