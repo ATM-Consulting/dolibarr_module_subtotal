20ae228920a376c87ff78d97dc7007f63aeed991
Jonathan Lescaut <jonathan.lescaut@atm-consulting.fr>
2023-12-01 09:21:36 +0100

    retour Pr phpmin et vDoli

--- core/modules/modSubtotal.class.php
+++ core/modules/modSubtotal.class.php
@@ -152,9 +152,9 @@ class modSubtotal extends DolibarrModules
         // List of modules id to disable if this one is disabled
         $this->requiredby = array();
         // Minimum version of PHP required by module
-        $this->phpmin = array(5, 4);
+        $this->phpmin = array(7,0);
         // Minimum version of Dolibarr required by module
-        $this->need_dolibarr_version = array(7, 0);
+        $this->need_dolibarr_version = array(15,0);
         $this->langfiles = array("subtotal@subtotal"); // langfiles@titre
         // Constants
         // List of particular constants to add when module is enabled
