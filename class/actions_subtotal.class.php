c6affeaf39f60aeb423c905aa38bae2926b5a05d
Ulysse Valdenaire <uvaldenaire@easya.solutions>
2024-06-04 14:08:47 +0200

    fix pdf transaction

--- class/actions_subtotal.class.php
+++ class/actions_subtotal.class.php
@@ -2162,6 +2162,7 @@ class ActionsSubtotal extends \subtotal\RetroCompatCommonHookActions
 		if(!empty($object->context)){ $object->context = array(); }
 		$object->context['subtotalPdfModelInfo'] = new stdClass(); // see defineColumnFiel method in this class
 		$object->context['subtotalPdfModelInfo']->cols = false;
+		$object->context['subtotalTransactionLine'] = array();
 
 
 
@@ -2407,7 +2408,10 @@ class ActionsSubtotal extends \subtotal\RetroCompatCommonHookActions
 						 * si aucune transaction n'est en cours, on peut y faire appel sans problème pour revenir
 						 * à l'état d'origine.
 						 */
-						$pdf->rollbackTransaction(true);
+						if (!isset($object->context['subtotalTransactionLine'][$i])) {
+							$pdf->rollbackTransaction(true);
+							$object->context['subtotalTransactionLine'][$i]  = true;
+						}
 						$pdf->startTransaction();
 
 						$pageBefore = $pdf->getPage();
@@ -2465,7 +2469,10 @@ class ActionsSubtotal extends \subtotal\RetroCompatCommonHookActions
 						 * si aucune transaction n'est en cours, on peut y faire appel sans problème pour revenir
 						 * à l'état d'origine.
 						 */
-						$pdf->rollbackTransaction(true);
+						if (!isset($object->context['subtotalTransactionLine'][$i])) {
+							$pdf->rollbackTransaction(true);
+							$object->context['subtotalTransactionLine'][$i]  = true;
+						}
 						$pdf->startTransaction();
 
 						$pageBefore = $pdf->getPage();
@@ -2512,7 +2519,10 @@ class ActionsSubtotal extends \subtotal\RetroCompatCommonHookActions
 						 * si aucune transaction n'est en cours, on peut y faire appel sans problème pour revenir
 						 * à l'état d'origine.
 						 */
-						$pdf->rollbackTransaction(true);
+						if (!isset($object->context['subtotalTransactionLine'][$i])) {
+							$pdf->rollbackTransaction(true);
+							$object->context['subtotalTransactionLine'][$i]  = true;
+						}
 						$pdf->startTransaction();
 
 						$pageBefore = $pdf->getPage();
