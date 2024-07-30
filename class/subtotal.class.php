73c52280fcdb232d6f962e8e0940231f9d1ae275
Benjamin Falière <benjamin.faliere@altairis.fr>
2024-07-30 15:33:24 +0200

    FIX(supplier): order and invoice subtotal lines

--- class/subtotal.class.php
+++ class/subtotal.class.php
@@ -141,7 +141,11 @@ class TSubtotal {
                 /** @var FactureFournisseur $object */
 			    $object->special_code = TSubtotal::$module_number;
                 if( (float)DOL_VERSION < 6 ) $rang = $object->line_max() + 1;
-			    $res = $object->addline($label,0,0,0,0,$qty,0,0,'','',0,0,'HT',9,$rang,false,0,null,0,0,'',TSubtotal::$module_number);
+				if ((int)DOL_VERSION < 17) {
+					$res = $object->addline($label,0,0,0,0,$qty, 0,0,'','', 0, '', 'HT', 9, $rang, false, 0, null, 0,0,'',TSubtotal::$module_number, 0,0);
+				} else {
+					$res = $object->addline($label,0,0,0,0,$qty,0,0,'','',0,0,'HT',9,$rang);
+				}
 			}
 			/**
 			 * @var $object Propal
@@ -160,8 +164,13 @@ class TSubtotal {
 			 * @var $object Commande fournisseur
 			 */
 			else if($object->element=='order_supplier') {
-				$object->special_code = TSubtotal::$module_number; // à garder pour la rétrocompatibilité
-			    $res = $object->addline($label, 0,$qty,0,0,0,0,0,'',0,'HT', 0, 9, 0, false, null, null, 0, null, 0, '', 0, -1, TSubtotal::$module_number);
+				/** @var CommandeFournisseur $object */
+			    $object->special_code = TSubtotal::$module_number;
+				if ((int)DOL_VERSION < 17) {
+					$res = $object->addline($label, 0, $qty, 0, 0, 0, 0, 0, '', 0, 'HT', 0, 9, 0, false, null, null, 0, null, 0, '', 0 ,$rang, TSubtotal::$module_number);
+				} else {
+					$res = $object->addline($label, 0,$qty,0,0,0,0,0,'',0,'HT', 0, 9);
+				}
 			}
 			/**
 			 * @var $object Facturerec
